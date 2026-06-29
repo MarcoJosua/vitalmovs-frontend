129ae4f5f69d5b647f057ffc0af962a55e4254bf
algrada <132857653+algrada@users.noreply.github.com>
2026-06-28 22:43:11 -0500

    Create Fisioterapeuta-service.ts

---
+++ src/app/services/Fisioterapeuta-service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Fisioterapeuta } from '../models/fisioterapeuta.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FisioterapeutaService {
+
+  private baseUrl = 'http://localhost:8080/vitalmovs';
+
+  constructor(private http: HttpClient) {}
+
+  listAll(): Observable<Fisioterapeuta[]> {
+    return this.http.get<Fisioterapeuta[]>(`${this.baseUrl}/fisioterapeuta/fisioterapeutas`);
+  }
+
+  add(fisioterapeuta: Fisioterapeuta): Observable<Fisioterapeuta> {
+    return this.http.post<Fisioterapeuta>(`${this.baseUrl}/fisioterapeuta`, fisioterapeuta);
+  }
+
+  update(fisioterapeuta: Fisioterapeuta): Observable<Fisioterapeuta> {
+    return this.http.put<Fisioterapeuta>(`${this.baseUrl}/fisioterapeuta`, fisioterapeuta);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/fisioterapeuta/${id}`);
+  }
+
+  buscarPorNombreOApellido(texto: string): Observable<Fisioterapeuta[]> {
+    const params = new HttpParams().set('texto', texto);
+    return this.http.get<Fisioterapeuta[]>(`${this.baseUrl}/fisioterapeuta/buscar`, { params });
+  }
+
+  findByEspecialidad(especialidad: string): Observable<Fisioterapeuta[]> {
+    const params = new HttpParams().set('especialidad', especialidad);
+    return this.http.get<Fisioterapeuta[]>(`${this.baseUrl}/fisioterapeuta/especialidad`, { params });
+  }
+}
