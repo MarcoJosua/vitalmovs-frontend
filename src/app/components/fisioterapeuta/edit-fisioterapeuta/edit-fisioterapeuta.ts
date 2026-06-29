6b9a49aa27791a156ecc0e4f9d188cc616775b40
algrada <132857653+algrada@users.noreply.github.com>
2026-06-28 23:31:31 -0500

    Create edit-fisioterapeuta.ts

---
+++ src/app/components/fisioterapeuta/edit-fisioterapeuta/edit-fisioterapeuta.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fisioterapeuta } from '../../models/fisioterapeuta.model';
+import { FisioterapeutaService } from '../../services/fisioterapeuta.service';
+
+@Component({
+  selector: 'app-edit-fisioterapeuta',
+  templateUrl: './edit-fisioterapeuta.component.html'
+})
+export class EditFisioterapeutaComponent implements OnInit {
+
+  fisioterapeuta: Fisioterapeuta = {
+    nombre: '',
+    apellido: '',
+    especialidad: '',
+    userId: 0
+  };
+
+  constructor(
+    private fisioterapeutaService: FisioterapeutaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.fisioterapeutaService.listAll().subscribe({
+      next: (data) => {
+        const encontrado = data.find(f => f.id === id);
+        if (encontrado) {
+          this.fisioterapeuta = { ...encontrado };
+        }
+      },
+      error: (err) => console.error('Error al cargar fisioterapeuta', err)
+    });
+  }
+
+  guardar(): void {
+    this.fisioterapeutaService.update(this.fisioterapeuta).subscribe({
+      next: () => this.router.navigate(['/fisioterapeutas']),
+      error: (err) => console.error('Error al actualizar', err)
+    });
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/fisioterapeutas']);
+  }
+}
