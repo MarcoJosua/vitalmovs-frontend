b91e4f7fca0a54f607daf5089afd2f166296d1a2
algrada <132857653+algrada@users.noreply.github.com>
2026-06-28 23:29:22 -0500

    Create list-fisioterapeuta.ts

---
+++ src/app/components/fisioterapeuta/list-fisioterapeuta/list-fisioterapeuta.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Fisioterapeuta } from '../../models/fisioterapeuta.model';
+import { FisioterapeutaService } from '../../services/fisioterapeuta.service';
+
+@Component({
+  selector: 'app-list-fisioterapeuta',
+  templateUrl: './list-fisioterapeuta.component.html'
+})
+export class ListFisioterapeutaComponent implements OnInit {
+
+  fisioterapeutas: Fisioterapeuta[] = [];
+
+  constructor(
+    private fisioterapeutaService: FisioterapeutaService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.cargarFisioterapeutas();
+  }
+
+  cargarFisioterapeutas(): void {
+    this.fisioterapeutaService.listAll().subscribe({
+      next: (data) => this.fisioterapeutas = data,
+      error: (err) => console.error('Error al cargar fisioterapeutas', err)
+    });
+  }
+
+  editar(id: number): void {
+    this.router.navigate(['/fisioterapeutas/editar', id]);
+  }
+
+  verDiscapacidades(id: number): void {
+    this.router.navigate(['/fisioterapeutas', id, 'discapacidades']);
+  }
+
+  eliminar(id: number): void {
+    if (confirm('¿Seguro que deseas eliminar este fisioterapeuta?')) {
+      this.fisioterapeutaService.delete(id).subscribe({
+        next: () => this.cargarFisioterapeutas(),
+        error: (err) => console.error('Error al eliminar', err)
+      });
+    }
+  }
+}
