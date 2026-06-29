9a371db3495f5adaee6b151a7d4ad28f0d787317
JoseLr422 <raperoelement@gmail.com>
2026-06-28 22:35:49 -0500

    Update Tipo-Discapacidad-services.ts

--- src/app/services/Tipo-Discapacidad-services.ts
+++ src/app/services/Tipo-Discapacidad-services.ts
@@ -1,1 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TipoDiscapacidad } from '../models/tipo-discapacidad';
 
+@Injectable({ providedIn: 'root' })
+export class TipoDiscapacidadService {
+  private baseUrl = 'http://localhost:8080/vitalmovs';
+
+  constructor(private http: HttpClient) {}
+
+  listAll(): Observable<TipoDiscapacidad[]> {
+    return this.http.get<TipoDiscapacidad[]>(`${this.baseUrl}/tipoDiscapacidad/tipos`);
+  }
+
+  add(tipo: TipoDiscapacidad): Observable<TipoDiscapacidad> {
+    return this.http.post<TipoDiscapacidad>(`${this.baseUrl}/TipoDiscapacidad`, tipo);
+  }
+
+  update(tipo: TipoDiscapacidad): Observable<TipoDiscapacidad> {
+    return this.http.put<TipoDiscapacidad>(`${this.baseUrl}/TipoDiscapacidad`, tipo);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/TipoDiscapacidad/${id}`);
+  }
+
+  buscarPorNombre(nombre: string): Observable<TipoDiscapacidad[]> {
+    return this.http.get<TipoDiscapacidad[]>(`${this.baseUrl}/tipoDiscapacidad/buscarNombre/${nombre}`);
+  }
+
+  buscarPorDescripcion(keyword: string): Observable<TipoDiscapacidad[]> {
+    return this.http.get<TipoDiscapacidad[]>(`${this.baseUrl}/tipoDiscapacidad/buscarDescripcion/${keyword}`);
+  }
+}
