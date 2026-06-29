7e7fe66ba2ec91f052130410d7f15f748c0e65b6
JoseLr422 <raperoelement@gmail.com>
2026-06-28 22:38:43 -0500

    Create Paciente-Discapacidad-services.ts

---
+++ src/app/services/Paciente-Discapacidad-services.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PacienteDiscapacidad } from '../models/paciente-discapacidad';
+
+@Injectable({ providedIn: 'root' })
+export class PacienteDiscapacidadService {
+  private baseUrl = 'http://localhost:8080/vitalmovs';
+
+  constructor(private http: HttpClient) {}
+
+  findByPacienteId(pacienteId: number): Observable<PacienteDiscapacidad[]> {
+    return this.http.get<PacienteDiscapacidad[]>(`${this.baseUrl}/pacienteDiscapacidad/paciente/${pacienteId}`);
+  }
+
+  add(pd: PacienteDiscapacidad): Observable<PacienteDiscapacidad> {
+    return this.http.post<PacienteDiscapacidad>(`${this.baseUrl}/PacienteDiscapacidad`, pd);
+  }
+
+  update(pd: PacienteDiscapacidad): Observable<PacienteDiscapacidad> {
+    return this.http.put<PacienteDiscapacidad>(`${this.baseUrl}/PacienteDiscapacidad`, pd);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/PacienteDiscapacidad/${id}`);
+  }
+
+  countByTipo(tipoId: number): Observable<number> {
+    return this.http.get<number>(`${this.baseUrl}/pacienteDiscapacidad/contar/${tipoId}`);
+  }
+}
