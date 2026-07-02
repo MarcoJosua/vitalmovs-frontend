ca007c1c1e466f3f91f83796d3e3525ae8082a43
JoseLr422 <raperoelement@gmail.com>
2026-07-02 12:51:00 -0500

    Update list-fisioterapeuta.ts

--- src/app/components/fisioterapeuta/list-fisioterapeuta/list-fisioterapeuta.ts
+++ src/app/components/fisioterapeuta/list-fisioterapeuta/list-fisioterapeuta.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Fisioterapeuta } from '../../models/fisioterapeutaDTO';
-import { FisioterapeutaService } from '../../services/fisioterapeuta-service';
+import { Fisioterapeuta } from '../../models/FisioterapeutaDTO';
+import { FisioterapeutaService } from '../../services/Fisioterapeuta-service';
 
 @Component({
   selector: 'app-list-fisioterapeuta',
